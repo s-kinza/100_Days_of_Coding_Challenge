6a0d8c421e9be3375c7eb219695853fa4b3bcdd2
syeda kinza <162494036+s-kinza@users.noreply.github.com>
2024-05-08 09:24:26 -0700

    Add files via upload

---
+++ Day 14/Question 41/main.js
@@ -0,0 +1,10 @@
+"use strict";
+//----------------------------QUESTION 41---------------------------
+// WELL COME TO MY CODING CHALLENGE
+let magicians = ["kinza", "areeba", "hamza"];
+function show_magicians(magicians) {
+    magicians.forEach(magicians => {
+        console.log(magicians);
+    });
+}
+show_magicians(magicians);
---
+++ Day 14/Question 41/main.ts
@@ -0,0 +1,10 @@
+//----------------------------QUESTION 41---------------------------
+// WELL COME TO MY CODING CHALLENGE
+let magicians : string[] = ["kinza", "areeba", "hamza"];
+ 
+function show_magicians(magicians: string[]) {
+    magicians.forEach(magicians =>{
+        console.log(magicians);
+    });
+}
+show_magicians(magicians);
